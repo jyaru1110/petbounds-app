afb1989f1eaa8f5bdc7ab569001aec16cec4fabe
Jyaru Hernandez <jyaru1110@gmail.com>
2021-05-29 23:36:09 -0500

    add mascota sin funcionalidad

--- src/HomeOrg.js
+++ src/HomeOrg.js
@@ -190,6 +190,21 @@ function Cuerpo(props) {
               </span>
             </Link>
           </div>
+          <div class="col d-flex justify-content-center contenido-org">
+                <form>
+                    <div class="form-group d-flex justify-content-center">
+                        <div class="d-flex d-md-flex justify-content-center align-items-center justify-content-md-center align-items-md-center div-input-file-a"><input class="form-control-file file" type="file" id="foto-perrito"/><label for="foto-perrito"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-plus-square">
+                                    <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
+                                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                                </svg></label>
+                        </div>
+                    </div><input class="form-control input-add" type="text" placeholder="Nombre mascota"/><input class="form-control input-add" type="text" placeholder="Raza"/><input class="form-control input-add" type="text"  placeholder="Animal"/>
+                    <div class="form-group edad-genero"><input class="form-control" type="text" placeholder="Edad"/><select class="form-control" style={{color: 'rgba(255, 255, 255, 0.616)'}}>
+                            <option value="Hembra" selected="">Hembra</option>
+                            <option value="Macho">Macho</option>
+                        </select></div><textarea class="form-control" id="text-area-add" placeholder="Descripción"></textarea><button class="btn btn-primary submit-add" type="submit">Registrar</button>
+                </form>
+            </div>
         </div>
       </div>
     );
